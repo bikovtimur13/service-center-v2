f46506a80583d00ba875e258c327e3b3be62f20c
Timur Bykov (4538) <bikov.timur.013@gmail.com>
2024-03-22 11:26:39 +0300

    main page desktop

--- scripts/main-page.js
+++ scripts/main-page.js
@@ -461,3 +461,25 @@ document.addEventListener('DOMContentLoaded', () => {
 //             });
 //     })
 // }) 
+
+
+//CATEGORIES SWIPER INIT
+const categoriesSwiper = () => {
+    const swiperWrapper = document.querySelector('.service-center__categories');
+    if(swiperWrapper){
+        const swiper = new Swiper(swiperWrapper, {
+            speed: 400,
+            loop: true,
+            slidesPerView: 3.5,
+            centeredSlides: true,
+            spaceBetween: 5,
+        });
+        swiper.autoplay.stop();
+    };
+}
+
+try{
+    categoriesSwiper();
+}catch(err){
+    console.warn(err);
+}
